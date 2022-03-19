2e20e56f1c526efd25dfe8f0027714abfa9c9bab
kamilbanas85 <53495965+kamilbanas85@users.noreply.github.com>
2022-03-19 01:22:28 +0100

    Create Sql_basic.sql

---
+++ Sql_basic.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Texas_Electricity_Demand2 (
+    [Date]     VARCHAR(30)  NOT NULL PRIMARY KEY,
+    Demand   Decimal(8,1) )
+
+
+# delate table
+DROP TABLE [dbo].[tableName]
+
+
+# delate all rows
+TRUNCATE TABLE [dbo].[tableName]
