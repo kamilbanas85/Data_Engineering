349c1fee21cd1ad0fafc9b684ce9298d5f725519
kamilbanas85 <53495965+kamilbanas85@users.noreply.github.com>
2022-04-21 09:40:56 +0200

    Update SQL_admin_commands.sql

--- SQL_admin_commands.sql
+++ SQL_admin_commands.sql
@@ -6,16 +6,34 @@ ALTER ROLE db_datareader ADD MEMBER [DashboardProba003];
 GO
 
 
---------------------------------------------------------
--- check users and them permissions
 
-select name as username,
-       create_date,
-       modify_date,
-       type_desc as type,
-       authentication_type_desc as authentication_type
-from sys.database_principals
-where type not in ('A', 'G', 'R', 'X')
-      and sid is not null
-      and name != 'guest'
-order by username;
+##############################
+### check perimssion 
+
+## check user permission
+
+SELECT  pri.name As Username
+,       pri.type_desc AS [User Type]
+,       permit.permission_name AS [Permission]
+,       permit.state_desc AS [Permission State]
+,       permit.class_desc Class
+,       object_name(permit.major_id) AS [Object Name]
+FROM    sys.database_principals pri
+LEFT JOIN
+        sys.database_permissions permit
+ON      permit.grantee_principal_id = pri.principal_id
+
+
+##########
+### however when user has db_writer database role it dosn't be listed with command above
+### below command shows userrs with database role
+
+SELECT DP1.name AS DatabaseRoleName,   
+   isnull (DP2.name, 'No members') AS DatabaseUserName   
+ FROM sys.database_role_members AS DRM  
+ RIGHT OUTER JOIN sys.database_principals AS DP1  
+   ON DRM.role_principal_id = DP1.principal_id  
+ LEFT OUTER JOIN sys.database_principals AS DP2  
+   ON DRM.member_principal_id = DP2.principal_id  
+WHERE DP1.type = 'R'
+ORDER BY DP1.name; 
