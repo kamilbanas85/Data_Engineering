d8634fa6e14a1bf9c0b4b8f48b73e66beb0df33c
kamilbanas85 <53495965+kamilbanas85@users.noreply.github.com>
2022-05-18 23:27:35 +0200

    Update Sql_basic.sql

--- Sql_basic.sql
+++ Sql_basic.sql
@@ -14,3 +14,18 @@ DROP TABLE [dbo].[tableName]
 
 -- delate all rows
 TRUNCATE TABLE [dbo].[tableName]
+
+
+-- insert into Table1 from Table2:
+INSERT INTO [schemaName].[Table1] (
+	   [col1]
+      ,[col2] )
+SELECT [col1]
+      ,[col2] 
+FROM [schemaName].[Table2]
+
+
+-- delate rows from Table1 duplicated in Table2 based on come column:
+DELETE t1 
+FROM [schemaName].[Table1] as t1
+INNER JOIN [schemaName].[Table2] as t2 ON t1.ColNameInTable1 = t2.ColNameInTable2;
