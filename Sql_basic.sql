9baac74da5e9628837be286814c3811cde963b50
kamilbanas85 <53495965+kamilbanas85@users.noreply.github.com>
2022-05-19 21:08:38 +0200

    Update Sql_basic.sql

--- Sql_basic.sql
+++ Sql_basic.sql
@@ -5,7 +5,8 @@ CREATE SCHEMA schema_name
 --
 CREATE TABLE Texas_Electricity_Demand2 (
     [Date]     VARCHAR(30)  NOT NULL PRIMARY KEY,
-    Demand   Decimal(8,1) )
+    Demand   Decimal(8,1) 
+    )
 
 
 -- delate table
@@ -16,7 +17,7 @@ DROP TABLE [dbo].[tableName]
 TRUNCATE TABLE [dbo].[tableName]
 
 
--- insert into Table1 from Table2:
+-- Insert into Table1 from Table2:
 INSERT INTO [schemaName].[Table1] (
        [col1]
       ,[col2] )
@@ -25,9 +26,18 @@ SELECT [col1]
 FROM [schemaName].[Table2]
 
 
--- delate rows from Table1 duplicated in Table2 based on come column:
+-- Delate rows based on condition
+DELETE  from [scheaName].[TableName]
+where colunmName = 'value'
+
+
+-- Delate rows from Table1 duplicated in Table2 based on come column:
 DELETE t1 
 FROM [schemaName].[Table1] as t1
 INNER JOIN [schemaName].[Table2] as t2 ON t1.ColNameInTable1 = t2.ColNameInTable2;
 
 
+-- Update values
+UPDATE [scheaName].[TableName]
+SET column1 = value1, column2 = value2, ...
+WHERE condition; 
