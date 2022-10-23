ba9b7c206ce4b52e12a32a340d8a95d3bee3e787
kamilbanas85 <53495965+kamilbanas85@users.noreply.github.com>
2022-10-23 13:24:23 +0200

    Update API.py

--- Extract_Data/API.py
+++ Extract_Data/API.py
@@ -1,18 +1,74 @@
+import numpy as np
+import pandas as pd
 import requests
 import json
-import pandas as pd
+import time
 
 #%%
 
-url = 'https://agsi.gie.eu/api/data/53XPL000000OSMP5/PL'
-headers = {'content-type': 'application/json', 'x-key': 'cf6b5bb569c70093205b81415cdcb32f'}
+# import os
+# MainDirectory = os.path.abspath(os.path.dirname(__file__))
+# os.chdir(MainDirectory)
 
-r = requests.get(url,  headers=headers)
+#%% Call For last Data
+
+headers = {"x-api-key" : "xxx"}
+
+# Country = 'de'
+CountryList = ['eu',
+               'at',
+               'be',
+               'bg',
+               'hr',
+               'cz',
+               'dk',
+               'de',
+               'es',
+               'fr',
+               'gb',
+               'hu',
+               'ie',
+               'it',
+               'lv',
+               'nl',
+               'pl',
+               'pt',
+               'ro',
+               'se',
+               'sk',
+               'rs',
+               'ua',
+               'ne']
+
+size = 300
+numberAPIcall = 0
+AgsiDataList = []
+
+for Country in CountryList:
 
 
-# Convert from JSON to a Python object (dictionary - a list that contains a single element, and that element is a dict):
-r_Text = json.loads(r.text)
+    CuntryURL_CurrentPage = f"https://agsi.gie.eu/api?country={Country}&size={size}"
+    try:
+        
+        resp = requests.get(url=CuntryURL_CurrentPage, headers=headers)
+        numberAPIcall = numberAPIcall + 1
+        try:
+          if len(resp.json()['data']) > 0:
+              AgsiDataList.append( pd.read_json( json.dumps( resp.json()['data'] ) ) )
+              Last_page_nr = resp.json()['last_page']
+          else:
+              # print('no data 0')
+              pass
+        except KeyError:
+           # print('no data')
+           pass
+    except requests.exceptions.RequestException as e:
+        print(f'error in API for country: {Country}')
+        pass
+    
+    if numberAPIcall == 60:
+        time.sleep(61)
+        numberAPIcall = 0
 
-res = pd.io.json.json_normalize(r_Text)
 
-r_df = pd.DataFrame(res)
+AgsiLastDataDF = pd.concat(AgsiDataList).sort_values(['code','gasDayStart']).reset_index(drop=True)
